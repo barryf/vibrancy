22586ed12aad53c7d760b583fa3ce7da52016581
Barry Frost <public@barryfrost.com>
2020-06-21 21:58:20 +0100

    Render post with Nunjucks and Markdown.

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,17 +1,26 @@
 const arc = require('@architect/functions')
+const nunjucks = require('nunjucks')
+const markdown = require('nunjucks-markdown')
+const marked = require('marked')
+
+const njkEnv = nunjucks.configure('views')
+markdown.register(njkEnv, marked)
 
 exports.handler = async function http (req) {
   if (req.path === '/favicon.ico') { return }
-
-  const slug = req.path.substr(1)
+  const slug = req.path.substr(1).replace('staging/','')
+  
   const data = await arc.tables()
   const post = await data.posts.get({slug})
+  const properties = JSON.parse(post.properties)
+  
+  const html = nunjucks.render('post.njk', properties)
 
   return {
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/plain; charset=utf8'
+      'content-type': 'text/html; charset=utf8'
     },
-    body: `${post.properties.content[0]}`
+    body: html
   }
 }
