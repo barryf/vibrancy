5afdd5af2cb249329ec224894146790560e7b543
Barry Frost <public@barryfrost.com>
2020-06-21 21:59:13 +0100

    Fix bug with storing data.

--- src/queues/upsert/index.js
+++ src/queues/upsert/index.js
@@ -1,14 +1,12 @@
 // `upsert`
 // * Creates/updates post in DynamoDB via MF2 JSON in message
-// * Columns for slug, kind, content, properties
+// * Columns for slug, published, kind, properties
 // * Sends SQS `ping` messages with new/updated URL
 
 const arc = require('@architect/functions')
 
 exports.handler = async function queue (event) {
   const data = await arc.tables()
-  event.Records.forEach(async record => {
-    const post = JSON.parse(record.body)
-    await data.posts.put(post)
-  })
+  const post = JSON.parse(event.Records[0].body)
+  await data.posts.put(post)
 }
