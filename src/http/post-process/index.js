e37eabdc15d1431335cdfa40f48d4488879d9a43
Barry Frost <public@barryfrost.com>
2020-07-02 21:48:34 +0100

    Ignore Vibrancy posts; check signature from GH

--- src/http/post-process/index.js
+++ src/http/post-process/index.js
@@ -1,41 +1,50 @@
 const arc = require('@architect/functions')
+const crypto = require('crypto')
 
-async function postExists (slug) {
-  const data = await arc.tables()
-  const post = await data.posts.get({ slug })
-  return post.slug === slug
+function verifySignature (signature, payload) {
+  const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET)
+  const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
+  const checksum = Buffer.from(signature, 'utf8')
+  return (checksum.length !== digest.length ||
+    !crypto.timingSafeEqual(digest, checksum))
 }
 
 async function queueDownloads (commits) {
   const slugs = []
   commits.forEach(async commit => {
-    ['added', 'modified'].forEach(async method => {
-      commit[method].forEach(async file => {
-        const slug = file.slice(0, -3)
-        // don't upsert post if we've already added it (via micropub)
-        if (method === 'added' && await postExists(slug)) {
-          console.log(`Ignoring adding ${slug} because it already exists.`)
-        } else {
+    // don't process if this commit has come via micropub
+    if (commit.message.match(/Vibrancy$/) === null) {
+      ['added', 'modified'].forEach(async method => {
+        commit[method].forEach(async file => {
+          const slug = file.slice(0, -3)
           slugs.push(slug)
           const payload = { slug, method }
           await arc.queues.publish({ name: 'download', payload })
-        }
+        })
       })
-    })
+    }
   })
   return slugs
 }
 
 exports.handler = async function http (req) {
   const body = arc.http.helpers.bodyParser(req)
-  // console.log(`process req=${JSON.stringify(body)}`)
 
-  // TODO check HTTP_X_HUB_SIGNATURE
+  // check signature from github matches secret
+  if ('X-Hub-Signature' in req.headers &&
+    verifySignature(req.headers['X-Hub-Signature'], JSON.stringify(body))) {
+    return {
+      statusCode: 403,
+      body: JSON.stringify({
+        error: 'forbidden',
+        message: 'GitHub webhook signatures did not match.'
+      })
+    }
+  }
 
   const slugs = await queueDownloads(body.commits)
   const message = slugs.length > 0 ? `Queued download of ${slugs.join(', ')}`
     : 'No files were added/modified.'
-
   return {
     statusCode: 202,
     body: JSON.stringify({ message })
