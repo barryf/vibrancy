b6ba4f41ebea70e144cb02b4aae320244a9f5e0c
Barry Frost <public@barryfrost.com>
2020-07-09 21:47:07 +0100

    No need to add table key

--- scripts/sandbox-startup.js
+++ scripts/sandbox-startup.js
@@ -2,10 +2,8 @@ const arc = require('@architect/functions')
 
 async function startup () {
   const data = await arc.tables()
-  const table = 'posts'
   const posts = [
     {
-      table,
       slug: '2020/07/foo',
       published: '2020-07-09T16:17:00',
       'post-type': 'note',
