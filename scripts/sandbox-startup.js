76f1f7536dc43b9654ed86a023f51c76bb9dc7d6
Barry Frost <public@barryfrost.com>
2020-07-09 16:35:01 +0100

    Startup script with some dummy data

---
+++ scripts/sandbox-startup.js
@@ -0,0 +1,19 @@
+const arc = require('@architect/functions')
+
+async function startup () {
+  const data = await arc.tables()
+  const table = 'posts'
+  const posts = [
+    {
+      table,
+      slug: '2020/07/foo',
+      published: '2020-07-09T16:17:00',
+      'post-type': 'note',
+      content: 'Foo'
+    }
+  ]
+  posts.forEach(async post => {
+    await data.posts.put(post)
+  })
+}
+module.exports = startup
