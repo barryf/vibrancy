3f172d1f86f9ff763b97a21674023294badbc4d3
Barry Frost <public@barryfrost.com>
2020-07-24 16:42:01 +0100

    Add post-types ?q= response

--- src/http/get-micropub/config-query.js
+++ src/http/get-micropub/config-query.js
@@ -1,9 +1,41 @@
-const q = `
-  syndicate-to
-  config
-  source
-`.trim().split(/\s+/)
-// TODO: contact, category
+const postTypes = [
+  {
+    type: 'note',
+    name: 'Note'
+  },
+  {
+    type: 'article',
+    name: 'Article'
+  },
+  {
+    type: 'bookmark',
+    name: 'Bookmark'
+  },
+  {
+    type: 'photo',
+    name: 'Photo'
+  },
+  {
+    type: 'checkin',
+    name: 'Checkin'
+  },
+  {
+    type: 'repost',
+    name: 'Repost'
+  },
+  {
+    type: 'like',
+    name: 'Like'
+  },
+  {
+    type: 'reply',
+    name: 'Reply'
+  },
+  {
+    type: 'rsvp',
+    name: 'RSVP'
+  }
+]
 
 const syndicateTo = [
   {
@@ -16,8 +48,17 @@ const targets = {
   'syndicate-to': syndicateTo
 }
 
+const q = [
+  'syndicate-to',
+  'config',
+  'source',
+  'post-types'
+]
+// TODO contact, category
+
 const config = {
   'media-endpoint': process.env.MEDIA_ENDPOINT_URL,
+  'post-types': postTypes,
   'syndicate-to': syndicateTo,
   q
 }
