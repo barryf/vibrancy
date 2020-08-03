62e235eb7d8c3f176e98598476e15ef58bf06cc4
Barry Frost <public@barryfrost.com>
2020-08-03 14:01:50 +0100

    WIP S3 uploading

--- src/http/post-media/index.js
+++ src/http/post-media/index.js
@@ -1,12 +1,32 @@
 const aws = require('aws-sdk')
 const arc = require('@architect/functions')
+// const { auth } = require('@architect/shared/auth')
 
-async function upload (body) {
-  return 'https://www.bbc.co.uk/'
+const s3 = new aws.S3({
+  endpoint: new AWS.Endpoint(process.env.S3_ENDPOINT)
+})
+
+async function upload (filename, file) {
+  s3.upload({
+    Bucket: process.env.S3_BUCKET,
+    ACL: 'public-read',
+    Key: filename,
+    Body: file
+  }, function (err, data) {
+    if (err) {
+      console.error(err)
+    } else if (data) {
+      return data.Location
+    }
+  })
 }
 
 exports.handler = async function http (req) {
   const body = arc.http.helpers.bodyParser(req)
+  console.log(`body=${JSON.stringify(body)}`)
+
+  // const authResponse = await auth.requireScope('media', req.headers, body)
+  // if (authResponse.statusCode >= 400) return authResponse
 
   const url = await upload(body)
   return {
