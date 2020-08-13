5f8aa180f9656f3f79d1e9ae294ee939b21c724e
Barry Frost <public@barryfrost.com>
2020-08-13 10:12:10 +0100

    Remove ping queue

--- src/queues/ping/index.js
+++
@@ -1,7 +0,0 @@
-// learn more about queue functions here: https://arc.codes/primitives/queues
-exports.handler = async function queue (event) {
-  console.log(JSON.stringify(event, null, 2))
-
-  # https://telegraph.p3k.io/api
-  # POST to https://telegraph.p3k.io/webmention
-}
