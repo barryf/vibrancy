f0fea22a1a6ab8ea7ab7cde6e96c20065b8407e1
Barry Frost <public@barryfrost.com>
2020-08-23 16:12:44 +0100

    Use twit for Twitter syndication

--- src/queues/syndicate/index.js
+++ src/queues/syndicate/index.js
@@ -5,20 +5,19 @@ exports.handler = async function queue (event) {
   const data = await arc.tables()
   const body = JSON.parse(event.Records[0].body)
   const url = body.url
-  // console.log('syn', body)
 
   if (body.syndicateTo && Array.isArray(body.syndicateTo)) {
     const post = await data.posts.get({ url })
     if (!('syndication' in post)) { post.syndication = [] }
     // iterate over each syndicateTo url
-    body.syndicateTo.forEach(async syndicateTo => {
+    console.log('body', body)
+    for (const syndicateTo of body.syndicateTo) {
       if (syndicateTo.indexOf('twitter.com') > -1) {
         const tweetUrl = await twitter.syndicate(post)
-        if (tweetUrl) {
-          post.syndication.push(tweetUrl)
-        }
+        post.syndication.push(tweetUrl)
       }
-    })
+    }
     await data.posts.put(post)
+    // TODO: update github file
   }
 }
