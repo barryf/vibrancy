2b5c068f48b550c861c34886d46e64f430962bd3
Barry Frost <public@barryfrost.com>
2020-09-06 21:44:19 +0100

    Media endpoint - store and query

--- src/http/get-media/index.js
+++ src/http/get-media/index.js
@@ -1,3 +1,45 @@
+const arc = require('@architect/functions')
+const { auth } = require('@architect/shared/auth')
+
+async function query (params) {
+  const data = await arc.tables()
+  const opts = {
+    IndexName: 'type-published-index',
+    ScanIndexForward: false,
+    KeyConditionExpression: '#type = :type',
+    ExpressionAttributeNames: {
+      '#type': 'type'
+    },
+    ExpressionAttributeValues: {
+      ':type': 'image'
+    }
+  }
+  let limit = 'limit' in params ? parseInt(params.limit, 10) : 20
+  if (!limit || limit < 1) limit = 1
+  opts.Limit = limit
+  return await data.media.query(opts)
+}
+
+async function source (params) {
+  const mediaData = await query(params)
+  const items = mediaData.Items
+  return {
+    body: JSON.stringify({ items })
+  }
+}
+
 exports.handler = async function http (req) {
-  return { body: 'media' }
+  // const body = arc.http.helpers.bodyParser(req)
+  // const authResponse = await auth.requireScope('media', req.headers, body)
+  // if (process.env.NODE_ENV === 'production' &&
+  //   authResponse.statusCode !== 200) return authResponse
+
+  const params = req.queryStringParameters || {}
+  if ('q' in params && params.q === 'source') {
+    return await source(params)
+  }
+  return {
+    headers: { 'Content-Type': 'text/plain; charset=utf8' },
+    body: 'Micropub media endpoint'
+  }
 }
