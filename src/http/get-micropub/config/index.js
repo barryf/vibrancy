1de9d6d3c93cd26bc4841728255b37a8bd864baf
Barry Frost <public@barryfrost.com>
2020-09-14 21:27:48 +0100

    Channels in q=config

--- src/http/get-micropub/config/index.js
+++ src/http/get-micropub/config/index.js
@@ -2,18 +2,31 @@ const syndicateTo = require('./syndicate-to')
 const category = require('./category')
 const postTypes = require('./post-types')
 
+const channels = [
+  {
+    uid: 'posts',
+    name: 'Posts'
+  },
+  {
+    uid: 'pages',
+    name: 'Pages'
+  }
+]
+
 const q = [
   'syndicate-to',
   'config',
   'source',
   'post-types',
-  'category'
+  'category',
+  'channels'
 ]
 
 const config = {
   'media-endpoint': process.env.MEDIA_ENDPOINT_URL,
   'post-types': postTypes,
   'syndicate-to': syndicateTo(),
+  channels,
   q
 }
 
