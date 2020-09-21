9f0002f251b80ec240412ee04393120014d06780
Barry Frost <public@barryfrost.com>
2020-09-21 21:53:56 +0100

    Media upload to Cloudinary

--- src/http/post-media/index.js
+++ src/http/post-media/index.js
@@ -1,11 +1,12 @@
-const AWS = require('aws-sdk')
+const cloudinary = require('cloudinary').v2
 const arc = require('@architect/functions')
+const parser = require('lambda-multipart-parser')
 // const { auth } = require('@architect/shared/auth')
 
-const s3 = new AWS.S3({
-  // endpoint: new AWS.Endpoint(process.env.S3_ENDPOINT)
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 function dateFilePath (published) {
@@ -15,39 +16,33 @@ function dateFilePath (published) {
   return `${yyyy}/${mm}/`
 }
 
-async function upload (file, filename) {
-  const ext = filename.split('.').slice(-1)[0]
-  const published = new Date()
-  const key = dateFilePath(published) +
+async function upload (file, contentType) {
+  const publishedDate = new Date()
+  const key = dateFilePath(publishedDate) +
     Math.random().toString(36).substring(2, 15)
-  s3.upload({
-    Bucket: process.env.MEDIA_BUCKET,
-    ACL: 'public-read',
-    Key: key,
-    Body: file
-  }, function (err, data) {
-    if (err) {
-      console.error(err)
-    } else if (data) {
-      console.log('location', data.Location)
-      return {
-        type: 'image', // TODO: other file types?
-        url: `${process.env.MEDIA_URL}${key}.${ext}`,
-        published
-      }
-    }
-  })
+  const dataUri = `data:${contentType};base64,${file}`
+  const result = await cloudinary.uploader.upload(dataUri, { public_id: key })
+  console.log('result', result)
+  return {
+    url: result.secure_url,
+    type: result.resource_type,
+    published: publishedDate.toISOString()
+  }
 }
 
 exports.handler = async function http (req) {
   const data = await arc.tables()
-  const body = arc.http.helpers.bodyParser(req)
 
   // const authResponse = await auth.requireScope('media', req.headers, body)
   // if (authResponse.statusCode >= 400) return authResponse
 
-  const media = await upload(body)
+  const result = await parser.parse(req)
+  const buffer = result.files[0].content
+  const file = Buffer.from(buffer).toString('base64')
+  const contentType = result.files[0].contentType
+  const media = await upload(file, contentType)
 
+  console.log('media', media)
   await data.media.put(media)
 
   return {
