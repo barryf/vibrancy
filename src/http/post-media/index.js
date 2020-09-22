e64ce68046ab61e8f66694254121322063f8a05b
Barry Frost <public@barryfrost.com>
2020-09-22 21:32:35 +0100

    Re-enable auth code

--- src/http/post-media/index.js
+++ src/http/post-media/index.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2
 const arc = require('@architect/functions')
 const parser = require('lambda-multipart-parser')
-// const { auth } = require('@architect/shared/auth')
+const { auth } = require('@architect/shared/auth')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -32,10 +32,11 @@ async function upload (file, contentType) {
 }
 
 exports.handler = async function http (req) {
+  const body = arc.http.helpers.bodyParser(req)
   const data = await arc.tables()
 
-  // const authResponse = await auth.requireScope('media', req.headers, body)
-  // if (authResponse.statusCode >= 400) return authResponse
+  const authResponse = await auth.requireScope('media', req.headers, body)
+  if (authResponse.statusCode >= 400) return authResponse
 
   const result = await parser.parse(req)
   const buffer = result.files[0].content
