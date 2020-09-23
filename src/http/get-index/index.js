9b7c0c00e976dcd98558f92081ec34c357ff2703
Barry Frost <public@barryfrost.com>
2020-09-23 21:51:42 +0100

    Simplify index

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,32 +1,7 @@
 exports.handler = async function http (req) {
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html; charset=utf8'
-    },
-    body: `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body>
-        <h1>Vibrancy Micropub server</h1>
-        <ul>
-          <li>
-            <a href="https://github.com/barryf/vibrancy">
-              https://github.com/barryf/vibrancy
-            </a>
-          </li>
-          <li>
-            <a href="https://micropub.net">
-              https://micropub.net
-            </a>
-          </li>
-        </ul>
-      </body>
-    </html>
-`
+    headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+    body: 'Vibrancy'
   }
 }
