d7180f482418f6d2141fc3eed45e19a6ab5cae47
Barry Frost <public@barryfrost.com>
2020-11-08 11:34:06 +0000

    Convert send-webmentions from queue to event.

--- src/events/send-webmentions/index.rb
+++ src/events/send-webmentions/index.rb
@@ -1,7 +1,7 @@
 require 'webmention'
 
 def handler(event)
-  body = JSON.parse(event['Records'][0]['body'])
+  body = JSON.parse(event['Records'][0]['Sns']['Message'])
   url = ENV['ROOT_URL'] + body['url']
   Webmention::Client.new(url).send_all_mentions
 end
