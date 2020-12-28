252d61203b971537cb0d9f8fb32e9d59212833af
Barry Frost <public@barryfrost.com>
2020-12-28 12:26:21 +0000

    Use @remy/webmention for sending

--- src/events/send-webmentions/index.js
+++ src/events/send-webmentions/index.js
@@ -1,6 +1,14 @@
-const arc = require('@architect/functions')
+const Webmention = require('@remy/webmention')
+const events = require('events')
+
+async function sendWebmentions (url, limit = 0, send = true) {
+  const wm = new Webmention({ limit, send })
+  wm.fetch(url)
+  await events.once(wm, 'end')
+  console.log('Dispatched', wm.endpoints)
+}
 
 exports.handler = async function subscribe (event) {
   const body = JSON.parse(event.Records[0].Sns.Message)
-
+  await sendWebmentions(body.url)
 }
