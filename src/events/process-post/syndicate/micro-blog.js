e4ceaa3ce4cd1dd40ae2f3596031801a0d9d1f5c
Barry Frost <public@barryfrost.com>
2021-02-20 17:44:52 +0000

    Fix body post format

--- src/events/process-post/syndicate/micro-blog.js
+++ src/events/process-post/syndicate/micro-blog.js
@@ -1,12 +1,13 @@
 const fetch = require('node-fetch')
 
 async function syndicate (post) {
+  const body = {
+    type: ['h-entry'],
+    properties: post.properties
+  }
   const response = await fetch('https://micro.blog/micropub', {
-    method: 'post',
-    body: {
-      type: ['h-entry'],
-      properties: post.properties
-    },
+    method: 'POST',
+    body: new URLSearchParams(body),
     headers: {
       Authorization: `Bearer ${process.env.MICRO_BLOG_TOKEN}`
     }
