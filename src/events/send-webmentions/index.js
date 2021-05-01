f69727ac9190d72ad158f0b52558d4e01fc2618f
Barry Frost <public@barryfrost.com>
2021-05-01 19:40:45 +0100

    Use wm.mentions for count

--- src/events/send-webmentions/index.js
+++ src/events/send-webmentions/index.js
@@ -6,8 +6,8 @@ async function sendWebmentions (url, limit = 0, send = true) {
   const wm = new Webmention({ limit, send })
   wm.fetch(url)
   await events.once(wm, 'end')
-  if (Array.isArray(wm.endpoints) && wm.endpoints.length) {
-    logger.info(`Sent ${wm.endpoints} webmentions`, JSON.stringify(wm.endpoints, null, 2))
+  if (Array.isArray(wm.mentions) && wm.mentions.length) {
+    logger.info(`Sent ${wm.mentions.length} webmentions`)
   }
 }
 
