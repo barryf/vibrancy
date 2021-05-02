a221e378472eec885e196ffb9a0970a637d0ca21
Barry Frost <public@barryfrost.com>
2021-05-02 08:13:12 +0100

    Tighten up which links to webmention
    = under .h-entry, no localhost, valid links.
    Success response from Telegraph is 2xx.

--- src/events/send-webmentions/index.js
+++ src/events/send-webmentions/index.js
@@ -1,18 +1,24 @@
 const cheerio = require('cheerio')
 const fetch = require('node-fetch')
 const logger = require('@architect/shared/logger')
+const { isValidURL } = require('@architect/shared/utils')
 
 async function findLinks (url) {
   const response = await fetch(url)
   const content = await response.text()
   const $ = cheerio.load(content)
+  // find all <a> tags inside h-entry
   const allLinks = []
-  $('a').each((_, tag) => {
+  $('.h-entry a').each((_, tag) => {
     allLinks.push($(tag).attr('href'))
   })
-  // filter out relative links or links to root
-  const links = allLinks.filter(link => link.startsWith('http'))
-  logger.info(`Found ${links.length} links at ${url}`, links.join('\n'))
+  // filter out invalid links
+  const links = allLinks.filter(link =>
+    link.startsWith('http') &&
+    !link.startsWith('http://localhost') &&
+    isValidURL(link)
+  )
+  logger.info(`Found ${links.length} link(s) at ${url}`, links.join('\n'))
   return links
 }
 
@@ -27,7 +33,7 @@ async function sendWebmention (source, target) {
   })
   const json = await response.json()
   const message = JSON.stringify(json, null, 2)
-  if (response.statusCode === 201) {
+  if (response.statusCode < 300) {
     logger.info(`Queued webmention with Telegraph from ${source} to ${target}`, message)
   } else {
     logger.error(`Error received from Telegraph for ${source} to ${target}`, message)
