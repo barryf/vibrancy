a4f0e427f8595cd3d29958924c2988c56582f0e6
Jamie Tanna <jamie@jamietanna.co.uk>
2021-07-31 16:05:43 +0100

    Add Meetup/Eventbrite context retrieval
    As I RSVP to events on Meetup/Eventbrite, it'd be useful to have the
    Pipes that I've set up for retrieving metadata from them to be supported
    in `fetch-context`.
    
    These follow the format of Granary, but expose an `is..Url` helper so we
    can simplify the logic when retrieving context.
    
    Additionally, we only want to use them if properties are found for a
    lookup, otherwise still fall back to Granary/OpenGraph.

--- src/events/fetch-context/index.js
+++ src/events/fetch-context/index.js
@@ -1,16 +1,30 @@
 const arc = require('@architect/functions')
 const logger = require('@architect/shared/logger')
+const eventbrite = require('./eventbrite')
 const granary = require('./granary')
+const meetup = require('./meetup')
 const openGraph = require('./open-graph')
 
 async function getContext (url) {
-  // try granary and then use opengraph
+  // for specific sites, use custom parsing
+  if (meetup.isMeetupUrl(url)) {
+    const properties = await meetup.fetchContext(url)
+    if (properties) {
+      return properties
+    }
+  } else if (eventbrite.isEventbriteUrl(url)) {
+    const properties = await eventbrite.fetchContext(url)
+    if (properties) {
+      return properties
+    }
+  }
+  // otherwise fallback to Granary, and then OpenGraph
   const properties = await granary.fetchContext(url)
   if (properties) {
     return properties
-  } else {
-    return await openGraph.fetchContext(url)
   }
+
+  return await openGraph.fetchContext(url)
 }
 
 exports.handler = async function subscribe (event) {
