b5281074454e999ad86f0d0195f8570223882163
Barry Frost <public@barryfrost.com>
2021-08-11 20:56:49 +0100

    Cater for bookmarks without content

--- src/events/process-post/syndicate/pinboard.js
+++ src/events/process-post/syndicate/pinboard.js
@@ -6,8 +6,10 @@ async function syndicate (post) {
     auth_token: process.env.PINBOARD_AUTH_TOKEN,
     url: post.properties['bookmark-of'][0],
     description: post.properties.name[0],
-    extended: post.properties.content[0],
     dt: post.properties.published[0]
+  }
+  if ('content' in post.properties) {
+    opts.extended = post.properties.content[0]
   }
   if (post.properties.category && Array.isArray(post.properties.category)) {
     opts.tags = post.properties.category.map(cat => {
