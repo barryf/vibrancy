2056929f9235abdcf17b0f6a595c25468713a1fe
Barry Frost <public@barryfrost.com>
2023-03-24 17:20:48 +0000

    Support Mastodon replies

--- src/events/process-post/syndicate/mastodon.js
+++ src/events/process-post/syndicate/mastodon.js
@@ -2,23 +2,39 @@ const fetch = require('node-fetch')
 const crypto = require('crypto')
 const logger = require('@architect/shared/logger')
 
+const statusIdFromUrl = url => {
+  const statusId = url.split('/').pop()
+  return statusId
+}
+
 function generateStatus (post) {
   const absoluteUrl = new URL(post.url, process.env.ROOT_URL).href
+  let content = ''
   if (post.properties.summary) {
-    return post.properties.summary[0] + ' ' + absoluteUrl
+    content = post.properties.summary[0] + ' ' + absoluteUrl
   } else if (post.properties.name) {
-    return post.properties.name[0] + ' ' + absoluteUrl
+    content = post.properties.name[0] + ' ' + absoluteUrl
   } else if (post.properties.content &&
     typeof post.properties.content[0] === 'string') {
-    return post.properties.content[0]
-  } else if (post.properties['repost-of']) {
-    return post.properties['repost-of'][0]
+    content = post.properties.content[0]
   }
+
+  const status = {
+    status: content
+  }
+
+  // add in-reply-to if appropriate (we assume it will be first reply url)
+  if (post.properties['in-reply-to']) {
+    const statusId = statusIdFromUrl(post.properties['in-reply-to'][0])
+    status.in_reply_to_id = statusId
+  }
+
+  return status
 }
 
 async function syndicate (post) {
-  const text = generateStatus(post)
-  const key = crypto.createHash('md5').update(text).digest('hex')
+  const status = generateStatus(post)
+  const key = crypto.createHash('md5').update(status.status).digest('hex')
   const token = process.env.MASTODON_TOKEN
   const server = process.env.MASTODON_URL.split('@')[0]
   const response = await fetch(`${server}api/v1/statuses`, {
@@ -27,13 +43,11 @@ async function syndicate (post) {
       'Idempotency-Key': key,
       Authorization: `Bearer ${token}`
     },
-    body: new URLSearchParams({
-      status: text
-    })
+    body: new URLSearchParams(status)
   })
-  const status = await response.json()
-  logger.info('Mastodon response', JSON.stringify(status, null, 2))
-  const url = `${process.env.MASTODON_URL}/${status.id}`
+  const toot = await response.json()
+  logger.info('Mastodon response', JSON.stringify(toot, null, 2))
+  const url = `${process.env.MASTODON_URL}/${toot.id}`
   return url
 }
 
