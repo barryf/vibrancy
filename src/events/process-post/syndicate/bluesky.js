f6048e46a441b2d34a88ae412b77957b8e189895
Barry Frost <public@barryfrost.com>
2024-11-24 17:06:51 +0000

    Bluesky image uploads

--- src/events/process-post/syndicate/bluesky.js
+++ src/events/process-post/syndicate/bluesky.js
@@ -12,12 +12,42 @@ async function syndicate (post) {
 
   const richText = new RichText({ text: content })
   await richText.detectFacets(agent)
-
-  const res = await agent.post({
+  const richPost = {
     text: richText.text,
     facets: richText.facets
-  })
-  const id = res.uri.split('/').slice(-1)[0]
+  }
+
+  const images = []
+  if ('photo' in post.properties) {
+    const photos = post.properties.photo.slice(0, 4) // max 4
+    for (const photo of photos) {
+      const url = (typeof photo === 'string') ? photo : photo.value
+      const alt = (typeof photo === 'string') ? '' : photo.alt
+      const response = await fetch(url)
+      if (!response.ok) continue
+      const arrayBuffer = await response.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+      const { data } = await agent.uploadBlob(
+        buffer,
+        { encoding: 'image/jpeg' })
+      if (data) {
+        images.push({
+          alt,
+          image: data.blob
+        })
+      }
+    }
+  }
+  if (images.length) {
+    richPost.embed = {
+      $type: 'app.bsky.embed.images',
+      images
+    }
+  }
+
+  const response = await agent.post(richPost)
+  if (!response.ok) return
+  const id = response.uri.split('/').slice(-1)[0]
   const url = `https://bsky.app/profile/${identifier}/post/${id}`
   return url
 }
